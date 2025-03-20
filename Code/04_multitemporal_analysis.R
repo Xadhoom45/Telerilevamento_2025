f487f38563c156667da286c1a643f50cfcb3913c
Xadhoom45 <gab.stefani@icloud.com>
2025-03-20 14:25:52 +0100

    Update 04_multitemporal_analysis.R

--- Code/04_multitemporal_analysis.R
+++ Code/04_multitemporal_analysis.R
@@ -34,24 +34,4 @@ grdif = gr[[1]] - gr[[4]]
 dev.off() 
 plot(grdif)
 
-# Exporting data
-setwd("C:/Users/Utente/OneDrive/Desktop/Magistrale Geologia/Telerilevamento Geoecologico/file_export")
-getwd() # controllo che la working directory sia giusta
 
-plot(gr)
-
-png("greenland_output.png")
-plot(gr)
-dev.off()
-
-pdf("greenland_output.pdf")
-plot(gr)
-dev.off()
-
-pdf("difgreen.pdf")
-plot(grdif)
-dev.off()
-
-jpeg("difgreen.jpeg")
-plot(grdif)
-dev.off()
