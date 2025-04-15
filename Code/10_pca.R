3df3a00e999f2f93812d0a2808806faaefec4da7
Xadhoom45 <gab.stefani@icloud.com>
2025-04-15 17:03:12 +0200

    Create 10_pca.R

---
+++ Code/10_pca.R
@@ -0,0 +1,31 @@
+# R code fro Principal component analysis
+
+library(imageRy)
+library(terra)
+im.list()
+
+sent = im.import("sentinel.png")
+
+sent = flip(sent)
+plot(sent)
+
+# volgiamo eliminare la quarta banda che è inutile, vogliamo creare un subset
+
+sent = c(sent[[1]],sent[[2]],sent[[3]]) # sovrascriviamo ancora l'immagine precedente
+
+# NIR = band 1
+# red = band 2
+# green = band 3
+
+sentpca = im.pca(sent, n_samples=100000)
+
+tot = 77 + 53 + 6
+# 137
+
+# 72 : 137 = x : 100
+
+72*100/tot
+
+sdpc1 = focal(sentpca[[1]], w=c(3,3), fun="sd")
+plot(sdpc1)
+
