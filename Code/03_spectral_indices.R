293d94bfd5a98645f2f8b4b4101024e64bf0de7e
Xadhoom45 <gab.stefani@icloud.com>
2025-08-26 17:55:28 +0200

    Update 03_spectral_indices.R

--- Code/03_spectral_indices.R
+++ Code/03_spectral_indices.R
@@ -8,12 +8,13 @@ im.list()
 
 mato1992 = im.import("matogrosso_l5_1992219_lrg.jpg")
 mato1992 = flip(mato1992)
+plot(mato1992)
 
+# Bande dell'immagine mato1992
 # 1 = NIR
 # 2 = red
 # 3 = green
 
-plot(mato1992)
 im.plotRGB(mato1992, r=1, g=2, b=3)
 im.plotRGB(mato1992, r=2, g=1, b=3)
 im.plotRGB(mato1992, r=2, g=3, b=1)
@@ -31,14 +32,12 @@ im.plotRGB(mato2006, r=2, g=3, b=1, title="Mato Grosso 2006")
 plot(mato1992[[1]], col=inferno(100))
 plot(mato2006[[1]], col=inferno(100))
 
-# DVI: Difference Vegetation Index
-# E. coli
-# Caribe
+# DVI: Difference Vegetation Index  --> DVI = NIR - red
 
 # Tree:           NIR=255, red=0, DVI=NIR-red=255-0=255
 # Stressed tree:  NIR=100, red=20, DVI=NIR-red=100-20=80
 
-# calculate dvi for 1992
+# calculate dvi for 1992 --> Valore di ogni singola cella da 0 a 255 (8bit), creo una immagine
 dvi1992 = mato1992[[1]] - mato1992[[2]] # NIR - red
 plot(dvi1992)
 
