d5786f411a9740ed1ed58a861a9ab31dfa063e17
Xadhoom45 <gab.stefani@icloud.com>
2025-09-01 11:12:13 +0200

    Update 05_exporting_graphs.R

--- Code/05_exporting_graphs.R
+++ Code/05_exporting_graphs.R
@@ -1,10 +1,10 @@
 # This code helps exporting graphs to images
 
 # Exporting data
-setwd("C:/Users/Utente/OneDrive/Desktop/Magistrale Geologia/Telerilevamento Geoecologico/file_export")
+setwd("C:/Users/Utente/OneDrive/Desktop/Magistrale Geologia/Telerilevamento Geoecologico/file_export") # seleziono la working directory in cui voglio salvae il mio file
 getwd() # controllo che la working directory sia giusta
 
-png("greenland_output.png") # selezioni il formato in cui voglio esportarla e gli do il nome
+png("greenland_output.png") # selezioni il formato in cui voglio esportarla e gli do il nome, questo è il comando che mi permette di esportare l'immagine
 plot(gr) # poi plotto l'immagine che sono interessato a esportare
 dev.off()
 
